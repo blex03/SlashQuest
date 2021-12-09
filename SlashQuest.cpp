db0273c9d3f691540a90f92114995c62fa4f1ff3
Bryan Lecza <brylex0324@gmail.com>
2021-12-09 14:25:20 -0500

    update

--- SlashQuest.cpp
+++ SlashQuest.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-
+#include <string>
+#include <algorithm>
 
 int main(){
 
-
+	std::cout << "SLASH QUEST - BRYAN LECZA" << std::endl;
+	std::cout << "DECEMBER 9, 2021" << std::endl << std::endl;
+	
 	std::string name;
 	std::cout << "Enter your character's name: ";
 	std::getline(std::cin, name);
-	std::cout << "Hello " << name << std::endl;
+	transform(name.begin(), name.end(), name.begin(), ::toupper);
+	
+	std::cout << "Hello " << name << ", Welcome to Slash Quest" << std::endl;
 	
 
 }
